55382e1b40cc3d1ba7e93dd0ebd4ab5363983960
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 23:37:26 +0500

    feat(components): enhance TodoItem and TodoList for dynamic rendering
    Update `TodoItem` to accept `className`, `id`, `title`, and `isDone` props for better configurability. Refactor `TodoList` to use `TodoItem` dynamically, rendering todos with specific properties for improved flexibility and maintainability.

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,18 @@
-export const TodoItem = () => {
+export const TodoItem = ({ className = '', id, title, isDone }) => {
   return (
-    <li className="todo__item todo-item">
+    <li className={`todo-item ${className}`}>
       <input
         className="todo-item__checkbox"
-        id="task-1"
+        id={id}
         type="checkbox"
-        checked
+        checked={isDone}
+        readOnly
       />
       <label
         className="todo-item__label"
-        htmlFor="task-1"
+        htmlFor={id}
       >
-        Task 1
+        {title}
       </label>
       <button
         className="todo-item__delete-button"
