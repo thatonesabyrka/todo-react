ce8d658710c31905b784ae1bdde0ed8d19750742
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 22:36:57 +0500

    feat(components): extract TodoItem component for reuse
    Refactor `App.jsx` by replacing the inline todo item with a new `TodoItem` component. Added `TodoItem.jsx` and updated imports to enhance code structure and maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { AddTaskForm } from './components/AddTaskForm'
 import { SearchTaskForm } from './components/SearchTaskForm'
 import { TodoInfo } from './components/TodoInfo'
+import { TodoItem } from './components/TodoItem'
 
 export const App = () => {
   return (
@@ -10,41 +11,7 @@ export const App = () => {
       <SearchTaskForm />
       <TodoInfo />
       <ul className="todo__list">
-        <li className="todo__item todo-item">
-          <input
-            className="todo-item__checkbox"
-            id="task-1"
-            type="checkbox"
-            checked
-          />
-          <label
-            className="todo-item__label"
-            htmlFor="task-1"
-          >
-            Task 1
-          </label>
-          <button
-            className="todo-item__delete-button"
-            aria-label="Delete"
-            title="Delete"
-          >
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 5L5 15M5 5L15 15"
-                stroke="#757575"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </li>
+        <TodoItem />
         <li className="todo__item todo-item">
           <input
             className="todo-item__checkbox"
