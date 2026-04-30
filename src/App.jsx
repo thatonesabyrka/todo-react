e5a2e2c7f9c3bcbcd4d2ec62ee1f407237133c94
Zhaslanuly Sabyrzhan <thatonesabyrka@gmail.com>
2026-04-30 22:39:26 +0500

    feat(components): extract Todo component for better modularity
    Refactor `App.jsx` by replacing the inline todo structure with a new `Todo` component. Added `Todo.jsx` and updated imports to improve code organization and readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import { AddTaskForm } from './components/AddTaskForm'
-import { SearchTaskForm } from './components/SearchTaskForm'
-import { TodoInfo } from './components/TodoInfo'
-import { TodoList } from './components/TodoList'
+import { Todo } from './components/Todo'
 
 export const App = () => {
-  return (
-    <div className="todo">
-      <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm />
-      <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
-    </div>
-  )
+  return <Todo />
 }
